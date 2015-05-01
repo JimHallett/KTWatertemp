2e69453544484faf8ee4812c2d9b6c02a933ca87
JimHallett <jhallett@ewu.edu>
2015-05-01 15:52:34 -0700

    minor cleanup

--- Rscripts/waterQA.R
+++ Rscripts/waterQA.R
@@ -13,10 +13,6 @@ library(dplyr)
 
 CCATemp <- tbl_df(read.csv("CCA1.csv", stringsAsFactors=F))
 
-
-#CCATemp$Date <- mdy(CCATemp$Date)
-
-
 CCATemp$DateTime <- as.POSIXct(paste(CCATemp$Date, CCATemp$Time), format="%m/%d/%Y %I:%M:%S %p")
 
 plot(CCATemp$DateTime, CCATemp$Temp)
